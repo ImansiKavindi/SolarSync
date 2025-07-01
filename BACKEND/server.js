885d6f5fd10a5613cf5bce49b96b453aca0ebd0c
ImansiKavindi <kavindikulathunga203@gmail.com>
2025-07-02 00:45:43 +0530

    .

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -45,7 +45,7 @@ app.use('/api/auth', authRoutes);
 app.use('/api/clients', clientRoutes);
 //app.use('/api/employeedashboard', employeedashboardRoutes);
 
-console.log("🚀 authRoutes are successfully loaded!");
+//console.log("🚀 authRoutes are successfully loaded!");
 
 // MongoDB Connection
 const URL = process.env.MONGODB_URL;
